b27d8458538de859e5ab384c70110287e6719dc3
Mitchel Alejo <michalejo94@gmail.com>
2022-11-10 17:39:17 -0600

    Part1 complete

--- db/index.js
+++ db/index.js
@@ -10,7 +10,23 @@ async function getAllUsers() {
     return rows;
   }
 
+  async function createUser({ username, password }) {
+    try {
+      const result = await client.query(`
+        INSERT INTO users(username, password) 
+        VALUES($1, $2) 
+        ON CONFLICT (username) DO NOTHING 
+        RETURNING *;
+      `, [username, password]);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
 module.exports = {
   client,
   getAllUsers,
+  createUser
 }
