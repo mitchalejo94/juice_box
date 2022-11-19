a16c0bd99284871eceb4c8601e4afe3ec9454357
Mitchel Alejo <michalejo94@gmail.com>
2022-11-19 10:56:39 -0600

    created PATCH in posts

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ server.use("/api", apiRouter);
 const { client } = require("./db");
 client.connect();
 
+
+server.get('/add/:first/to/:second', (req, res, next) => {
+  res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
+    Number(req.params.first) + Number(req.params.second)
+   }</h1>`);
+});
+
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
 });
